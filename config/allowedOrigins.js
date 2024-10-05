149f0a262cad2964658c8778907060c47516ec20
Priyanshu Gupta <118977399+psgupta712@users.noreply.github.com>
2024-10-05 18:50:52 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://lambent-baklava-2b1e43.netlify.app'
+    'https://unrivaled-pastelito-12343d.netlify.app'
 
 ];
 
